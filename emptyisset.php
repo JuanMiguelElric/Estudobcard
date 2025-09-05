e0d386f7beaaf76419d1bf828a90c8b735bd18d7
Juan Miguel <oliveirajuan243@gmail.com>
2025-09-04 21:21:45 -0300

    estudo com isset

---
+++ emptyisset.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $var = "juan";
+    $idade;
+
+    if (isset($var)) {
+        echo "está definido ";
+        # code...
+    }
+
+    if(isset($idade)){
+        echo "está definida";
+    }else{
+        echo "não está definida";
+    }
+
+
+?>
