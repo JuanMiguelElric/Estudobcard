22819d1e46ca7bac3d8be8f1d6de275b59033fdd
Juan Miguel <oliveirajuan243@gmail.com>
2025-09-04 21:29:16 -0300

    estudo com empty

--- emptyisset.php
+++ emptyisset.php
@@ -5,13 +5,22 @@
 
     if (isset($var)) {
         echo "está definido ";
-        # code...
+
     }
+
+    //isset virifica se uma variável foi definida e portanto não é nula ela retorna true se a var existir e tiver um valor diferente de NULL
 
     if(isset($idade)){
         echo "está definida";
     }else{
-        echo "não está definida";
+        echo "não está definida ";
+    }
+
+    // verifica se a variavel foi definida, se o valor é === a zero ou se é vazio ou se é falso
+    if(empty($idade)){
+        echo " variavel não definida";
+    }else{
+        echo "variável definida";
     }
 
 
