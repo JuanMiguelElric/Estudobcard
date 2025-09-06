63c5ac4bfb2b12eee9cf2924391b5601ded51162
Juan Miguel <oliveirajuan243@gmail.com>
2025-09-05 23:27:09 -0300

    Estudo de ternário

---
+++ terna.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+$idade= 150;
+
+echo $idade <18 ? "menor": "audlto";
+?>
+
+
+$n=8;
+
+echo $n % 2 ? "impar":"par" ;
+
+$pedidoAprovado = true;
+echo $pedidoAprovado == true ? "Aprovado":"Negado";
+*/
+
+$nota = 90;
+
+echo ($nota >= 90)? "A":(($nota >= 60)? "B":"c");
